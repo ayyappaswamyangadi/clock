8098f43bb26478c2f61cc5f4c7861157039f4d45
Ayyappa Swamy Angadi <ayyappaswamy50@gmail.com>
2022-03-31 17:18:48 +0530

    source code
    pushed the complete code

---
+++ clock.js
@@ -0,0 +1,23 @@
+setInterval(setClock, 1000)
+
+const dataHourHand = document.querySelector('[data-hour-hand]')
+const dataMinuteHand = document.querySelector('[data-minute-hand]')
+const dataSecondHand = document.querySelector('[data-second-hand]')
+
+function setClock(){
+    const currentDate = new Date();
+    // console.log(currentDate)
+    const secondRatio = currentDate.getSeconds()/60;
+    const minuteRatio = currentDate.getMinutes()/60;
+    const hourRatio = currentDate.getHours()/12;
+
+    setRotation(dataSecondHand, secondRatio)
+    setRotation(dataMinuteHand, minuteRatio)
+    setRotation(dataHourHand, hourRatio)
+}
+
+function setRotation(element, rotationRatio){
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+setClock()
+
